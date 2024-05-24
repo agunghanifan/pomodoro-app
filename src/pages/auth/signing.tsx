4b1adb102536840698ee1e277ffe7a5cd754a799
agunghanifan <agung.hanifan@gmail.com>
2024-05-24 19:27:29 +0700

    feat(auth) : add validate session on /auth/signing page

--- src/pages/auth/signing.tsx
+++ src/pages/auth/signing.tsx
@@ -1,7 +1,18 @@
 // src/pages/auth/signing.tsx
-import { signIn } from 'next-auth/react';
+import {signIn, useSession} from 'next-auth/react';
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function Signing() {
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session) {
+            router.push('/protected').then();
+        }
+    }, [session, router]);
+
     return (
         <div>
             <h1>Sign In</h1>
